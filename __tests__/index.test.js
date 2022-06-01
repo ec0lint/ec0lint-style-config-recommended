f24c2e207cf4a9db86bc94ff11a57759a87e2a19
Anna Gut <gucieekx3@gmail.com>
2022-06-02 00:07:31 +0200

    config changed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@
 
 const config = require('../');
 const fs = require('fs');
-const stylelint = require('stylelint');
+const ec0lint_css = require('ec0lint-css');
 
 const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
 const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
@@ -11,7 +11,7 @@ describe('flags no warnings with valid css', () => {
 	let result;
 
 	beforeEach(() => {
-		result = stylelint.lint({
+		result = ec0lint_css.lint({
 			code: validCss,
 			config,
 		});
@@ -30,7 +30,7 @@ describe('flags warnings with invalid css', () => {
 	let result;
 
 	beforeEach(() => {
-		result = stylelint.lint({
+		result = ec0lint_css.lint({
 			code: invalidCss,
 			config,
 		});
@@ -42,31 +42,5 @@ describe('flags warnings with invalid css', () => {
 
 	it('flags one warning', () => {
 		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
-	});
-
-	it('correct warning text', () => {
-		return result.then((data) =>
-			expect(data.results[0].warnings[0].text).toBe(
-				'Unexpected unknown type selector "madeup" (selector-type-no-unknown)',
-			),
-		);
-	});
-
-	it('correct rule flagged', () => {
-		return result.then((data) =>
-			expect(data.results[0].warnings[0].rule).toBe('selector-type-no-unknown'),
-		);
-	});
-
-	it('correct severity flagged', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
-	});
-
-	it('correct line number', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(1));
-	});
-
-	it('correct column number', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(1));
 	});
 });
