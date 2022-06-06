43c28967f7c01e61167e97b5aab0fa0b50b843a4
malwinq <malwina.kubas@gmail.com>
2022-06-06 23:44:21 +0200

    release

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -47,7 +47,7 @@ describe('flags warnings with invalid css', () => {
 	it('correct warning text', () => {
 		return result.then((data) =>
 			expect(data.results[0].warnings[0].text).toBe(
-				'Format of custom font can be changed to WOFF or WOFF2. CO2 reduction: up to 80% of the font file. (no-ttf-font-files)',
+				'Format of the custom font can be changed to WOFF or WOFF2. CO2 reduction: up to 80% of the font file. (no-ttf-font-files)',
 			),
 		);
 	});
